a8cab4a98c3bb597b73bb9fa872f61a0ebeef741
Rhys Davies <rhys.davies901@gmail.com>
2016-06-15 15:59:13 +0100

    Removing the wd upstream so I can have this module in my repo for a bit

--- init.zsh
+++ init.zsh
@@ -1,7 +1,4 @@
 source `dirname $0`/rhys/init.zsh
 source `dirname $0`/dev_tools/init.zsh
 source `dirname $0`/git/init.zsh
-
-wd() {
-	  . /Users/daviesrh/bin/wd/wd.sh
-  }
+source `dirname $0`/wd/init.zsh

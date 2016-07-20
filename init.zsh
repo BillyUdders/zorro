6f7882442d5d171b8044065d123f20f1c02fb5dd
Rhys Davies <rhys.davies901@gmail.com>
2016-07-20 12:56:15 -0700

    Removing platform specific stuff - for the time being this is going to be ansiblised

--- init.zsh
+++ init.zsh
@@ -1,4 +1,3 @@
 source `dirname $0`/rhys/init.zsh
 source `dirname $0`/dev_tools/init.zsh
 source `dirname $0`/git/init.zsh
-source `dirname $0`/wd/init.zsh
--- rhys/init.zsh
+++ rhys/init.zsh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 #####################################################################################################
 #												    #	
 # This is my own personal repo of crud that I use to move around and make my life a little easier.  #
@@ -9,10 +11,17 @@
 alias c='clear'
 alias gs='git status'
 alias mud='git remote update && git merge upstream/develop'
-alias rm='rmtrash'
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
 alias zshr='source ~/.zshrc'
 alias l='la'
+
+#####################
+# We are all douchebags sometimes, just deal with it...
+if which rmtrash >/dev/null 2>&1; then
+  alias rm='rmtrash'
+else 
+  alias rm='trash-put'
+fi
 
 ######################
 # Additional Git Shit
@@ -29,7 +38,7 @@ bindkey '^R' history-incremental-search-backward
 
 ######################
 # Attaching to dev Docker box
-if [ ! -z "$PS1" ] && docker-machine status dev|grep -q Running; then
+if which docker-machine >/dev/null 2>&1 && [ ! -z "$PS1" ] && docker-machine status dev|grep -q Running; then
   eval $(docker-machine env dev)
 fi
 

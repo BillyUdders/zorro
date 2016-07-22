86cc967dd8f32a00240eba85b9479204c2bd1c0d
Rhys Davies <rhys.davies901@gmail.com>
2016-07-22 08:32:17 -0700

    Few ok - this is getting a bit gnarly. But now I can add directories with a naming convention and have them automatically seen by my .zshrc

--- init.zsh
+++ init.zsh
@@ -1,3 +1,5 @@
-source `dirname $0`/rhys/init.zsh
-source `dirname $0`/dev/init.zsh
-source `dirname $0`/git/init.zsh
+#!/bin/zsh
+
+for file in `dirname $0`/**/*alias*; do
+    source "$file"
+done

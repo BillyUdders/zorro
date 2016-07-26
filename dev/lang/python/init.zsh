021896da2614fc3bab5d1ac703d5b9f581ea1ae5
Rhys Davies <rhys.davies901@gmail.com>
2016-07-26 06:56:02 -0700

    Changing stuff around to make a little more sense

--- dev/lang/python/init.zsh
+++
@@ -1,9 +0,0 @@
-#######################
-# Python stuff, work in progress
-if which brew >/dev/null 2>&1; then
-  export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-fi
-
-
--- init.zsh
+++ init.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+LSB_DISTRIBUTION=(`eval lsb_release -si`)
+
 for file in `dirname $0`/**/*alias*; do
     source "$file"
 done
